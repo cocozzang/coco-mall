812949a86d186e27214bb61b003612b9e5f5a136
cocozzang <cocorlawnghks@gmail.com>
2023-10-25 21:42:20 +0900

    chore: supabaseJS setting for image storage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,5 +8,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export const supabase = createClient(
   "https://swaxqumvxaqwocxxczoy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3YXhxdW12eGFxd29jeHhjem95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0MTE5MjAsImV4cCI6MjAwOTk4NzkyMH0.fYfcj7QQ-_JUs5HsxWInJc7GSEHvmTwXiVs9fPGm9OI"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3YXhxdW12eGFxd29jeHhjem95Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDQxMTkyMCwiZXhwIjoyMDA5OTg3OTIwfQ.gFqmE64DgW1Ecvm4TuCjRsYFDd9uAnQ2LuejQQUR3wY"
 )
+
+export const GOODS_THUMBNAIL_URL =
+  "https://swaxqumvxaqwocxxczoy.supabase.co/storage/v1/object/public/goods_thumbnail/"
+export const GOODS_IMAGE_URL =
+  "https://swaxqumvxaqwocxxczoy.supabase.co/storage/v1/object/public/goods_image/"
+export const GOODS_CONTENT_IMAGE_URL =
+  "https://swaxqumvxaqwocxxczoy.supabase.co/storage/v1/object/public/goods_content_image/"
+export const REVIEW_IMAGE_URL =
+  "https://swaxqumvxaqwocxxczoy.supabase.co/storage/v1/object/public/review_image/"
